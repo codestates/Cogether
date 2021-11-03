4d6011996c3763d9a005935d362af35c4f13045d
LimJungMyeong <freewjdaud@gmail.com>
2021-11-03 17:27:28 +0900

    회원정보 수정에러 핸들

--- client/src/pages/Setting.js
+++ client/src/pages/Setting.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 import { setConfirmModal } from '../actions/index';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 import { setQuarterModal } from '../actions';
 import '../scss/Setting.scss';
 
 const Setting = () => {
   const dispatch = useDispatch();
-  const [userProfileImg, setUserProfileImg] = useState('');
+  const [userProfileImg, setUserProfileImg] = useState(null);
   const [file, setFile] = useState('');
   const history = useHistory();
 
@@ -30,6 +30,7 @@ const Setting = () => {
         console.log(data);
         setUserProfileImg(data.image);
         setUpdate({ ...update, nickname: data.nickname });
+        dispatch(setConfirmModal(false, ''));
       })
       .catch((err) => {
         console.log(err);
@@ -54,8 +55,8 @@ const Setting = () => {
   };
 
   const deleteImg = () => {
-    setUserProfileImg('');
-    setFile('');
+    setUserProfileImg(null);
+    setFile(null);
   };
 
   const handleInputValue = (key) => (e) => {
@@ -89,45 +90,49 @@ const Setting = () => {
         history.push('/');
       })
       .catch((err) => {
+        if (err.response.data.message === 'nickname is already exist') {
+          dispatch(setConfirmModal(true, '이미있는 닉네임 입니다.'));
+          window.location.replace(`/Setting`);
+        }
         console.log(err);
       });
   };
 
   return (
-    <div className='setting'>
-      <div className='setting-header'>
+    <div className="setting">
+      <div className="setting-header">
         <h2>회원 정보수정</h2>
       </div>
-      <div className='setting-img'>
+      <div className="setting-img">
         <img
-          className='profile'
+          className="profile"
           src={userProfileImg ? userProfileImg : './images/default-profile.jpg'}
         ></img>
       </div>
-      <div className='settig-profile'>
+      <div className="settig-profile">
         <label>
           프로필 사진 변경
           <input
-            className='hide'
-            type='file'
-            accept='image/jpg, image/jpeg, image/png'
+            className="hide"
+            type="file"
+            accept="image/jpg, image/jpeg, image/png"
             onChange={(e) => insertImg(e)}
           ></input>
         </label>
         <button onClick={deleteImg}>프로필 사진 제거</button>
       </div>
-      <div className='settig-nickname'>
+      <div className="settig-nickname">
         <p>닉네임 : </p>
         <input
           onChange={handleInputValue('nickname')}
           value={update.nickname}
         ></input>
       </div>
-      <div className='settig-password'>
+      <div className="settig-password">
         <p>비밀번호 : </p>
         <input onChange={handleInputValue('password')}></input>
       </div>
-      <div className='setting-userInfo'>
+      <div className="setting-userInfo">
         <button onClick={userInfoUpdate}>변경 완료</button>
         <button
           onClick={() => {
