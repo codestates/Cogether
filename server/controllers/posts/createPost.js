92954696e4dce7cb6a614275499799ee640f0199
SeheePark <seahee9390@gmail.com>
2021-11-04 17:31:05 +0900

    createPost.js : 게시글 작성 및 업로드 기능

--- server/controllers/posts/createPost.js
+++ server/controllers/posts/createPost.js
@@ -3,102 +3,58 @@ const { isAuthorized } = require('../../utils/helpFunc');
 
 module.exports = async (req, res) => {
   const { title, content, stacks } = req.body;
+  const auth = isAuthorized(req);
 
-  console.log(stacks.length);
+  if (!auth) {
+    return res.status(401).send({
+      message: 'unauthorized user',
+    });
+  }
 
-  // for (let i = 0; i < stacks.length; i++) {
-  //   Post_hashtag.create({
-  //     postId: 1,
-  //     hashtagId: stacks[i],
-  //   });
+  try {
+    const userInfo = await User.findOne({
+      where: {
+        id: auth.id,
+      },
+      attributes: ['nickname', 'image'],
+    });
+    // create post
 
-  await Post_hashtag.create({
-    hashtagId: 1,
-    postId: 1,
-  });
+    const post = await Post.create({
+      userId: userInfo.id,
+      nickname: userInfo.nickname,
+      title: title,
+      content: content,
+      stacks: stacks,
+    });
 
-  const find = await Post_hashtag.findAll();
+    // post_hashtag 생성
 
-  res.status(200).send({
-    data: find,
-  });
+    for (let i = 0; i < stacks.length; i++) {
+      Post_hashtag.create({
+        postId: post.id,
+        hashtagId: stacks[i],
+      });
+    }
 
-  // const stacks = stack.map((stack) => stack.value);
-  // [1, 2, 3, 4, 5];
-  // Post_hashtag.create({
-  //   postId: 1,
-  //   hashtagId: 3,
-  // });
-
-  // const hashtag1 = await Hashtag.findOne({
-  //   where: {
-  //     id: 1,
-  //   },
-  // });
-
-  // const hashtag2 = await Hashtag.findOne({
-  //   where: {
-  //     stack: stacks,
-  //   },
-  // });
-
-  // const posts1 = await hashtag1.getPosts();
-  // const posts2 = await hashtag2.getPosts();
-
-  // res.status(200).send({
-  //   data: {
-  //     posts1,
-  //     posts2,
-  //   },
-  // });
-
-  // const auth = isAuthorized(req);
-
-  // if (!auth) {
-  //   return res.status(401).send({
-  //     message: 'unauthorized user',
-  //   });
-  // }
-
-  // try {
-  //   const userInfo = await User.findOne({
-  //     where: {
-  //       id: auth.id,
-  //     },
-  //     attributes: ['nickname', 'image'],
-  //   });
-
-  //   const post = await Post.create({
-  //     userId: userInfo.id,
-  //     nickname: userInfo.nickname,
-  //     title: title,
-  //     content: content,
-  //     stacks: stacks,
-  //   });
-
-  //   const hashtags = await Post_hashtag.create({
-  //     // postId: post.id,
-  //     stacks: stacks,
-  //   });
-
-  //   res.status(200).send({
-  //     data: {
-  //       id: post.id,
-  //       userId: userInfo.id,
-  //       nickname: userInfo.nickname,
-  //       image: userInfo.image,
-  //       title: post.title,
-  //       content: post.content,
-  //       stacks: hashtags.stacks,
-  //       totalViews: post.totalViews,
-  //       totalInterests: post.totalInterests,
-  //       totalComments: post.totalComments,
-  //       updatedAt: post.updatedAt,
-  //       createdAt: post.createdAt,
-  //     },
-  //     message: 'create post successed',
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  // }
+    res.status(200).send({
+      data: {
+        id: post.id,
+        userId: userInfo.id,
+        nickname: userInfo.nickname,
+        image: userInfo.image,
+        title: post.title,
+        content: post.content,
+        stacks: stacks,
+        totalViews: post.totalViews,
+        totalInterests: post.totalInterests,
+        totalComments: post.totalComments,
+        updatedAt: post.updatedAt,
+        createdAt: post.createdAt,
+      },
+      message: 'create post successed',
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
