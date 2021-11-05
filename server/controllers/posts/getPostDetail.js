24ba0de9074f6b027c34eaf554105b8d02c6b7f5
SeheePark <seahee9390@gmail.com>
2021-11-05 15:51:07 +0900

    getPostDetail.js : 게시물 상세 조회 기능 구현

--- server/controllers/posts/getPostDetail.js
+++ server/controllers/posts/getPostDetail.js
@@ -1,3 +1,29 @@
-module.exports = (req, res) => {
-  res.status(200).send('ok');
+const { Post } = require('../../models');
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Post.findOne({
+      where: {
+        id,
+      },
+    });
+    // update totalViews value + 1
+    if (post) {
+      post.totalViews = post.totalViews + 1;
+    }
+
+    await post.save();
+
+    res.status(200).send({
+      data: post,
+      message: 'get post detail successed',
+    });
+  } catch (err) {
+    // update totalViews value - 1
+    post.totalViews = post.totalViews - 1;
+    await post.save();
+    console.log(err);
+  }
 };
