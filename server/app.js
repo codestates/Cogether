5a4497592cab2aba78f4bb18263f4ed7b2239db0
SeheePark <seahee9390@gmail.com>
2021-11-09 10:39:45 +0900

    app.js, server.js : 서버 구성 수정, DB 연결 확인 코드 추가

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const http = require('http');
-
-// routers declaration
-const { userRouter } = require('./routers/user');
-const { postRouter } = require('./routers/post');
-const { interestPostRouter } = require('./routers/interestPost');
-const { commentRouter } = require('./routers/comment');
-const { chatroomRouter } = require('./routers/chatroom');
-const { chattingRouter } = require('./routers/chatting');
-const { evaluationRouter } = require('./routers/evaluation');
-
 const app = express();
+const db = require('./models');
+
+// check database connection
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log('Connect to MySQL successed');
+  })
+  .catch(console.error);
+
+// app setting
 
 app.use(
   cors({
@@ -29,6 +30,16 @@ app.get('/', (req, res) => {
   res.status(200).send('Cogether Project !');
 });
 
+// routers declaration
+
+const { userRouter } = require('./routers/user');
+const { postRouter } = require('./routers/post');
+const { interestPostRouter } = require('./routers/interestPost');
+const { commentRouter } = require('./routers/comment');
+const { chatroomRouter } = require('./routers/chatroom');
+const { chattingRouter } = require('./routers/chatting');
+const { evaluationRouter } = require('./routers/evaluation');
+
 // express use routers
 
 app.use('/users', userRouter);
@@ -38,3 +49,5 @@ app.use('/comments', commentRouter);
 app.use('/chatrooms', chatroomRouter);
 app.use('/chattings', chattingRouter);
 app.use('/evaluations', evaluationRouter);
+
+module.exports = app;
