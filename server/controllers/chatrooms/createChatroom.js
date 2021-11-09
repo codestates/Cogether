26af2f56a3224987a40b33e1b70d825129f1d629
SeheePark <seahee9390@gmail.com>
2021-11-09 14:41:12 +0900

    채팅 관련 코드 추가

--- server/controllers/chatrooms/createChatroom.js
+++ server/controllers/chatrooms/createChatroom.js
@@ -1,3 +1,75 @@
+const { Chatroom, User } = require('../../models');
+const { isAuthorized } = require('../../utils/helpFunc');
+const _ = require('lodash');
+
 module.exports = (req, res) => {
-  res.status(200).send('ok');
+  const auth = isAuthorized(req);
+
+  if (!auth) {
+    return res.status(401).send({
+      message: 'unauthorized user',
+    });
+  }
+
+  const myId = auth.id;
+  const opponentId = req.body.opponentId;
+
+  try {
+    const user = await User.findOne({
+      where: {
+        id: auth.id,
+      },
+      attributes: ['id'],
+      include: [{ model: Chatroom }],
+    });
+
+    const oppentUser = await User.findOne({
+      where: {
+        id: opponentId,
+      },
+      attributes: ['id'],
+      include: [{ model: Chatroom }],
+    });
+
+    const myChatrooms = [];
+    const opponenUserList = [];
+
+    for (let i = 0; i < user.Chatrooms.length; i++) {
+      myChatrooms.push(user.Chatrooms[i].id);
+    }
+
+    for (let i = 0; i < oppentUser.Chatrooms.length; i++) {
+      myChatrooms.push(opponenUserList.Chatrooms[i].id);
+    }
+
+    const isChatroom = _.intersection(myChatrooms, opponenUserList);
+    console.log(isChatroom);
+
+    if (isChatroom.length === 0) {
+      const chatroomInfo = await Chatroom.create({});
+
+      await chatroomInfo.addUsers(myId);
+      await chatroomInfo.addUsers(opponentId);
+
+      return res.status(200).send({
+        data: {
+          chatroomId: chatroomInfo.id,
+          myId,
+          opponentId,
+        },
+        message: 'create chatroom successed',
+      });
+    } else if (isChatroom.length === 1) {
+      return res.status(200).send({
+        data: {
+          chatroomId: isChatroom[0],
+          myId,
+          opponentId,
+        },
+        message: 'create chatroom successed',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
--- server/controllers/chatrooms/getAllChatrooms.js
+++ server/controllers/chatrooms/getAllChatrooms.js
@@ -1,3 +1,76 @@
-module.exports = (req, res) => {
-  res.status(200).send('ok');
+const { User, Chatroom } = require('../../models');
+const { isAuthorized } = require('../../utils/helpFunc');
+
+module.exports = async (req, res) => {
+  console.log('read Chatroom List');
+  const auth = isAuthorized(req);
+
+  if (!auth) {
+    return res.status(401).send({
+      message: 'unauthorized user',
+    });
+  }
+
+  const myId = auth.id;
+  let data = [];
+
+  /*
+    roomid, = 룸 아이디
+    opponentId
+    opponentName = 상대방 닉네임
+    opponentimg = 상대방 프사
+    updatedAt = 마지막 채팅 시간
+  */
+
+  try {
+    const myInfo = await User.findOne({
+      where: {
+        id: myId,
+      },
+      include: [
+        {
+          model: Chatroom,
+        },
+      ],
+    });
+    // console.log(myInfo.id);
+    // console.log(myInfo.Chatrooms);
+    if (myInfo.Chatrooms.length !== 0) {
+      for (let i = 0; i < myInfo.Chatrooms.length; i++) {
+        if (myInfo.Chatrooms[i]) {
+          //   console.log(myInfo.Chatrooms[i].id);
+          const RoomInfo = await Chatroom.findOne({
+            where: {
+              id: myInfo.Chatrooms[i].id,
+            },
+            include: [{ model: User }],
+          });
+          //   console.log(RoomInfo.id);
+          //   console.log(RoomInfo.Users);
+          const opponentInfoList = RoomInfo.Users.filter(
+            (user) => user.id !== myId
+          );
+          //   console.log(opponentInfoList);
+          opponentInfoList.forEach((opponentInfo) => {
+            // console.log(opponentInfo);
+            data.push({
+              roomId: RoomInfo.id,
+              opponentId: opponentInfo.id,
+              opponentEmail: opponentInfo.email,
+              opponentNicknam: opponentInfo.nickname,
+              opponentProfileImg: opponentInfo.image,
+            });
+          });
+        }
+      }
+      // 시간.....
+      //   console.log(data);
+      return res.status(200).send({ data, message: '성공' });
+    } else {
+      return res.status(200).send({ message: '채팅목록없음' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('에러');
+  }
 };
