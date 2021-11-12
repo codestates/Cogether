564d002790eab63afc2c82c049daa5b7037010cf
SeheePark <seahee9390@gmail.com>
2021-11-13 00:32:58 +0900

    GoTop.js : 에러 핸들링

--- client/src/components/GoTop.js
+++ client/src/components/GoTop.js
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GoTop() {
   const [scrollPosition, setScrollPosition] = useState(window.scrollY);
   const handleScroll = () => {
     setScrollPosition(window.scrollY);
   };
-  window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       {scrollPosition === 0 ? null : scrollPosition > 980 ? (
         <i
-          className="fas fa-chevron-circle-up fa-3x topScroll"
+          className='fas fa-chevron-circle-up fa-3x topScroll'
           onClick={() =>
             window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
           }
         ></i>
       ) : (
         <i
-          className="fas fa-chevron-circle-up fa-3x"
+          className='fas fa-chevron-circle-up fa-3x'
           onClick={() =>
             window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
           }
